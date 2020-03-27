9e102a12cedb92126cc2e6c90b18118b6381191b
reireynoso <reireynoso@gmail.com>
2020-03-27 11:59:19 -0400

    filter

--- src/app.js
+++ src/app.js
@@ -10,12 +10,13 @@ import {setTextFilter} from './actions/filters'
 import visibleExpenses from './selectors/expenses'
 
 const store = configureStore();
-store.dispatch(addExpense({description: "water", amount: 200}))
-store.dispatch(addExpense({description: "gas", amount: 400}))
-store.dispatch(setTextFilter("water"))
-console.log(store.getState().expenses, store.getState().filters)
-let result = visibleExpenses(store.getState().expenses, store.getState().filters)
-console.log(result);
+store.dispatch(addExpense({description: "water", amount: 800, createdAt: 14}))
+store.dispatch(addExpense({description: "gas", amount: 400, createdAt: 19}))
+store.dispatch(addExpense({description: "food", amount: 1400, createdAt: 10}))
+// store.dispatch(setTextFilter("water"))
+// console.log(store.getState().expenses, store.getState().filters)
+// let result = visibleExpenses(store.getState().expenses, store.getState().filters)
+// console.log(result);
 
 
 const jsx = (
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {removeExpense} from '../actions/expenses'
 
-export default (props) => {
-    const {description, amount, createdAt} = props.expense
+const ExpenseListItem = (props) => {
+    const {id, description, amount, createdAt} = props.expense
     return (
     <div>
         <h3>{description}</h3>
         <p>{amount} - {createdAt}</p>
+        <button onClick={() => {
+            props.dispatch(removeExpense(id))
+        }}>Remove</button>
     </div>
     )
 }
+
+export default connect()(ExpenseListItem)
