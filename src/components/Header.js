c275de9976bcd4d0a40b6d4d942a1f081dc30ac9
reireynoso <reireynoso@gmail.com>
2020-03-29 17:44:44 -0400

    react-test-renderer with shallow implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ const Header = () => (
         <NavLink exact={true} to="/" activeClassName="is-active">Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
         <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
 )
 
