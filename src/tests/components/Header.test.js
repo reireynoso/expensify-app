250a3c17baee23809f12fd9ae43a487f1baf7f56
reireynoso <reireynoso@gmail.com>
2020-03-29 18:45:59 -0400

    enzyme installation

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,15 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+// import ReactShallowRenderer from 'react-test-renderer/shallow'
 import React from 'react'
 import Header from '../../components/Header'
+import {shallow} from 'enzyme';
+import {toJSON} from 'enzyme-to-json'
 
 test("Should render Header correctly", () => {
-    const renderer = new ReactShallowRenderer()
-    renderer.render(<Header/>)
-    expect(renderer.getRenderOutput()).toMatchSnapshot()
+    // const renderer = new ReactShallowRenderer()
+    // renderer.render(<Header/>)
+    // expect(renderer.getRenderOutput()).toMatchSnapshot()
+
+    const wrapper = shallow(<Header/>)
+    // expect(wrapper.find('h1').text()).toBe('Expensify')
+    expect(wrapper).toMatchSnapshot()
 })
