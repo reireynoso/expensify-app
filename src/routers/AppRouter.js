58ef0d5a7cb994c899d01374ae3ff3f7334e60b0
reireynoso <reireynoso@gmail.com>
2020-04-06 14:37:57 -0400

    added route back for edit

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
 import Header from '../components/Header'
 import AddExpensePage from '../components/AddExpensePage'
+import EditExpensePage from '../components/EditExpensePage'
 import HelpPage from '../components/HelpPage'
 import NotFoundPage from '../components/NotFound'
 
@@ -14,6 +15,7 @@ const AppRouter = () => (
             <Switch>
                 <Route path="/" component={ExpenseDashboardPage} exact={true}/>
                 <Route path="/create" component={AddExpensePage}/>
+                <Route path="/edit/:id" component={EditExpensePage}/>
                 <Route path="/help" component={HelpPage}/>
                 <Route component={NotFoundPage}/>
             </Switch>
