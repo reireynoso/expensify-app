e22958ff83650d7083318084c5ea5de0c6b3875d
reireynoso <reireynoso@gmail.com>
2020-04-09 19:48:25 -0400

    redirect with history npm

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider } from 'react-redux'
-import AppRouter from './routers/AppRouter'
+import AppRouter, {history} from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
@@ -27,19 +27,31 @@ const jsx = (
     </Provider>
 )
 
+let hasRendered = false;
+const renderApp = () => {
+    if(!hasRendered){
+        ReactDOM.render(
+            jsx, 
+            document.getElementById("app")
+        )
+        hasRendered = true; 
+    }
+}
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById("app"))
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(
-        jsx, 
-        document.getElementById("app")
-    ) 
-})
+
 
 firebase.auth().onAuthStateChanged((user) => {
     if(user){
-        console.log('login')
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp()
+            if(history.location.pathname === "/"){
+                history.push('/dashboard')
+            }
+        })
     }else{
-        console.log('logout')
+        renderApp()
+        history.push('/')
     }
 })
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Router, Route, Switch} from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
 import Header from '../components/Header'
 import AddExpensePage from '../components/AddExpensePage'
@@ -8,9 +9,10 @@ import HelpPage from '../components/HelpPage'
 import NotFoundPage from '../components/NotFound'
 import LoginPage  from '../components/LoginPage'
 
+export const history = createHistory()
 
 const AppRouter = () => (
-    <BrowserRouter>
+    <Router history = {history}>
         <div>
             <Header/>
             <Switch>
@@ -22,7 +24,7 @@ const AppRouter = () => (
                 <Route component={NotFoundPage}/>
             </Switch>
         </div>
-    </BrowserRouter>
+    </Router>
 )
 
 export default AppRouter
