2498edada6166482407c83ecfcb403bf6655c309
reireynoso <reireynoso@gmail.com>
2020-04-08 20:29:27 -0400

    fetching data and setting

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
-// import {addExpense} from './actions/expenses'
+import {startSetExpenses} from './actions/expenses'
 // import {setTextFilter} from './actions/filters'
 // import visibleExpenses from './selectors/expenses'
 import 'react-dates/lib/css/_datepicker.css'
@@ -26,8 +26,13 @@ const jsx = (
         <AppRouter/>
     </Provider>
 )
-ReactDOM.render(
-    jsx, 
-    document.getElementById("app")
-)
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById("app"))
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(
+        jsx, 
+        document.getElementById("app")
+    ) 
+})
 
