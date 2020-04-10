5d646c005017d2f68537846019d13405e3ff8e75
reireynoso <reireynoso@gmail.com>
2020-04-10 18:15:45 -0400

    styled login component

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,12 @@ import {startLogin} from '../actions/auth'
 export class LoginPage extends React.Component {
     render(){
         return (
-            <div>
-                <button onClick={this.props.startLogin}>Login</button>
+            <div className="box-layout">
+                <div className="box-layout__box">
+                    <h1 className="box-layout__title">Expensify App</h1>
+                    <p>Time to get expenses under control.</p>
+                    <button onClick={this.props.startLogin}>Login</button>
+                </div>
             </div>
         )
     }
