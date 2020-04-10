582a9c06e3463c39bbf6c16ed7819652fd5260ed
reireynoso <reireynoso@gmail.com>
2020-04-10 18:41:46 -0400

    style header component and restructured

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,9 +7,9 @@ export class LoginPage extends React.Component {
         return (
             <div className="box-layout">
                 <div className="box-layout__box">
-                    <h1 className="box-layout__title">Expensify App</h1>
+                    <h1 className="box-layout__title">Expensify</h1>
                     <p>Time to get expenses under control.</p>
-                    <button onClick={this.props.startLogin}>Login</button>
+                    <button className="button" onClick={this.props.startLogin}>Login with Google</button>
                 </div>
             </div>
         )
